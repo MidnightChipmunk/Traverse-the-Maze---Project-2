f5d4bc55acc92c991560e05326c77e3e5a16f4eb
King Maxwell <mmdawg1@aol.com>
2019-03-21 19:36:57 -0400

    MazeFromFile fixed
    The program should be working correctly now

--- src/MazeBuilder.java
+++ src/MazeBuilder.java
@@ -96,7 +96,6 @@ public class MazeBuilder {
 		//Add every available neighbor to each cell
 		for(int i = 0; i < rows; i++){
 			for(int j = 0; j < columns; j++){
-				System.out.println(grid[i][j]);
 				int ns = northSouth.nextInt();
 				int ew = eastWest.nextInt();
 				for(int k = 0; k < 4; k++){
@@ -135,7 +134,6 @@ public class MazeBuilder {
 						}
 						break;
 					}
-					System.out.println("Before" + grid[i][j].getNeighbor(k));
 				}
 				
 				//Remove neighbors based on the file
@@ -143,18 +141,21 @@ public class MazeBuilder {
 					switch(k) {
 					case 0:
 						if(ns == 1) {
-							grid[i][j].getNeighbor(2).addNeighbor(null, 0);
+							if(grid[i][j].getNeighbor(2) != null && i < rows-1) {
+								grid[i+1][j].addNeighbor(null, 0);
+							}
 							grid[i][j].addNeighbor(null, 2);
 						}
 						break;
 					case 1:
 						if(ew == 1) {
-							grid[i][j].getNeighbor(1).addNeighbor(null, 3);
+							if(grid[i][j].getNeighbor(1) != null && j > 0) {
+								grid[i][j-1].addNeighbor(null, 3);
+							}
 							grid[i][j].addNeighbor(null, 1);
 						}
 						break;
 					}
-					System.out.println("After: " + grid[i][j].getNeighbor(k));
 				}
 			}
 		}
--- src/TraverseTheMaze.java
+++ src/TraverseTheMaze.java
@@ -7,18 +7,30 @@ import javax.swing.JOptionPane;
 public class TraverseTheMaze {
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws FileNotFoundException {
-		Maze maze = new Maze((MazeBuilder.loadMazeFile()));
-
+		Maze maze = new Maze(MazeBuilder.loadMazeFile());
+		
 		maze.findPath(maze.getCellAt(0, 0));
+		displayMaze(maze);
+		
+		
+		/*for(int i = 0; i < 10; i++) {
+			maze = new Maze(MazeBuilder.loadMazeRandom());
+			maze.findPath(maze.getCellAt(0,0));
+			displayMaze(maze);
+			System.out.println("\n\n-------------------------------\n\n");
+		}*/
+		
+	}
 	
-		try{
+	public static void displayMaze(Maze maze) {
+		if(!maze.getStack().isEmpty()) {
 			if(maze.getStack().size() > 0){
 				JOptionPane.showMessageDialog(new JFrame(), "Path found, coordinates in the consoles!");
 				GenericNode.reverseData(maze.getStack().top);
 				maze.getStack().display();
 				maze.display();
 			}
-		}catch(Exception e){
+		}else{
 			JOptionPane.showMessageDialog(new JFrame(), "No path found!");
 		}
 	}
